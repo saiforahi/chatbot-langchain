8d1c7087ff86ddba22dbb842db0ced24c88a5097
saiforahi <shaif.rahi@northsouth.edu>
2024-06-06 13:07:08 +0600

    aws key removed

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 TEMP_FILE_DIR = "temp"
 FACE_DETECTIONS_RESULTS_DIR = "face-detections"
 AWS_DEFAULT_REGION = 'us-east-1'
-AWS_ACCESS_KEY_ID = 'AKIAXY7KTDYE4IWJUWN6'
+AWS_ACCESS_KEY_ID = 'abc'
 AWS_SECRET_ACCESS_KEY = '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'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)) 
 EMBEDDDING_ROOT = os.path.join('application', 'controllers', 'bot', 'static')
